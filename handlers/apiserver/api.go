e9db5751d6185d15571b01e54014d1db9cc07104
Adachi Sakura <805983409@qq.com>
2020-04-16 22:07:11 +0800

    get pod metrics, get node, deployment

--- handlers/apiserver/api.go
+++ handlers/apiserver/api.go
@@ -3,12 +3,13 @@ package apiserver
 import (
 	"context"
 	"github.com/buzaiguna/gok8s/appctx"
+	v1 "k8s.io/api/apps/v1"
 	"net/http"
 )
 
 func GetPods(ctx context.Context) error {
-	cli := appctx.K8SClient(ctx)
-	namespace := appctx.Param(ctx, "namespace")
+	cli := appctx.NewK8SClient(ctx)
+	namespace := appctx.Query(ctx, "namespace")
 	pods, err := cli.ListPods(namespace)
 	if err != nil {
 		return err
@@ -17,4 +18,28 @@ func GetPods(ctx context.Context) error {
 	return nil
 }
 
+func GetNode(ctx context.Context) error {
+	nodeName := appctx.Param(ctx, "nodeName")
+	cli := appctx.NewK8SClient(ctx)
+	node, err := cli.GetNode(nodeName)
+	if err != nil {
+		return err
+	}
+	appctx.JSON(ctx, http.StatusOK, node)
+	return nil
+}
+
+func CreateDeployment(ctx context.Context) error {
+	deployment := &v1.Deployment{}
+	if err := appctx.BindK8SYaml(ctx, deployment); err != nil {
+		return err
+	}
+	cli := appctx.NewK8SClient(ctx)
+	result, err := cli.CreateDeployment(deployment.Namespace, deployment)
+	if err != nil {
+		return err
+	}
+	appctx.JSON(ctx, http.StatusCreated, result)
+	return nil
+}
 
--- handlers/metrics/api.go
+++ handlers/metrics/api.go
@@ -9,24 +9,32 @@ import (
 
 func GetNodesMetrics(ctx context.Context) error {
 	cli := appctx.NewMetricsClient(ctx)
-	metrics, err := cli.ListNodeMetrics()
+	metricses, err := cli.ListNodeMetrics()
 	if err != nil {
 		return err
 	}
-	appctx.JSON(ctx, http.StatusOK, metrics)
+	appctx.JSON(ctx, http.StatusOK, metricses)
 	return nil
 }
 
 func GetNodeMetrics(ctx context.Context) error {
-	cli := appctx.NewMetricsClient(ctx)
 	nodeName := appctx.Param(ctx, "nodeName")
-	if nodeName == "" {
-		return apperror.NewParameterRequiredError("nodeName")
-	}
+	cli := appctx.NewMetricsClient(ctx)
 	metrics, err := cli.GetNodeMetrics(nodeName)
 	if err != nil {
 		return apperror.NewInvalidParameterError(nodeName, err)
 	}
 	appctx.JSON(ctx, http.StatusOK, metrics)
+	return nil
+}
+
+func GetPodsMetrics(ctx context.Context) error {
+	cli := appctx.NewMetricsClient(ctx)
+	namespace := appctx.Query(ctx, "namespace")
+	metricses, err := cli.ListPodMetrics(namespace)
+	if err != nil {
+		return err
+	}
+	appctx.JSON(ctx, http.StatusOK, metricses)
 	return nil
 }
