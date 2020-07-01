693b0b378553344e83f204976d1742b31dfaa93d
Adachi Sakura <805983409@qq.com>
2020-07-01 12:36:57 +0800

    chore(build): exit when script error

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 version=$1
 GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -installsuffix cgo -o bin/gok8s .
 # GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o bin/gok8s .
